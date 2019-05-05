e84037131f06577a14651acee197e9b14c364987
Ninh <ninhnguyen375@gmail.com>
2019-05-05 09:31:06 +0700

    init

--- crawler.js
+++ crawler.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
-const Crawler = require('crawler');
+const fs = require("fs");
+const Crawler = require("crawler");
 
-const diemK17FilePath = './db/DiemHe4_K17.json';
-const diemK18FilePath = './db/DiemHe4_K18.json';
-const diemK17FilePath__DTN = './db/DiemHe4_K17__DTN.json';
-const diemK18FilePath__DTN = './db/DiemHe4_K18__DTN.json';
+const diemK17FilePath = "./db/DiemHe4_K17.json";
+const diemK18FilePath = "./db/DiemHe4_K18.json";
+const diemK17FilePath__DTN = "./db/DiemHe4_K17__DTN.json";
+const diemK18FilePath__DTN = "./db/DiemHe4_K18__DTN.json";
 
 const diemK17Data = [];
 const diemK18Data = [];
@@ -13,7 +13,7 @@ const diemK18__DTN = [];
 
 let studentName = [];
 let diemHe4 = [];
-let diemHe4Cua1SV = '';
+let diemHe4Cua1SV = "";
 const urls = [];
 
 const c = new Crawler({
@@ -25,11 +25,11 @@ const c = new Crawler({
     } else {
       const $ = res.$;
       const data = {
-        mssv: res.options.uri.split('id=')[1],
+        mssv: res.options.uri.split("id=")[1],
         diem: parseFloat(
-          $('.row-diemTK')
+          $(".row-diemTK")
             .eq(3)
-            .find('.Label')
+            .find(".Label")
             .eq(1)
             .text()
         )
@@ -47,12 +47,12 @@ setTimeout(() => {
   fs.writeFile(diemK18FilePath, JSON.stringify(diemK18Data), err => {
     err && console.log(err);
   });
-  console.log('done');
+  console.log("done");
 }, 10000);
 
 for (let i = 3118410000; i < 3118410400; i++) {
   urls.push(
-    'http://thongtindaotao.sgu.edu.vn/Default.aspx?page=xemdiemthi&id=' + i
+    "http://thongtindaotao.sgu.edu.vn/Default.aspx?page=xemdiemthi&id=" + i
   );
 }
 
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 
-const fs = require('fs');
+const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'pug');
-app.use(express.static('public'));
+app.set("view engine", "pug");
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/api', async (req, res) => {
+app.get("/api", async (req, res) => {
   res.send(`
     <h4>API List</h4>
     <p>/api/diemhe4_k17</p>
@@ -24,7 +24,7 @@ app.get('/api', async (req, res) => {
 });
 
 const getStudent = ({ id, res, file }) => {
-  fs.readFile(file, 'utf-8', (err, data) => {
+  fs.readFile(file, "utf-8", (err, data) => {
     if (err) res.status(400).statusMessage(err.message);
     else {
       const myData = JSON.parse(data);
@@ -43,47 +43,47 @@ const getStudent = ({ id, res, file }) => {
 };
 
 const getStudents = ({ res, file }) => {
-  fs.readFile(file, 'utf-8', (err, data) => {
+  fs.readFile(file, "utf-8", (err, data) => {
     if (err) res.status(400).statusMessage(err.message);
     else res.json(JSON.parse(data));
   });
 };
 
 // APIs
-app.get('/api/diemhe4_k17/:id', async (req, res) => {
+app.get("/api/diemhe4_k17/:id", async (req, res) => {
   const { id } = req.params;
-  getStudent({ id, res, file: './db/DiemHe4_K17.json' });
+  getStudent({ id, res, file: "./db/DiemHe4_K17.json" });
 });
 
-app.get('/api/diemhe4_k17', async ({ res }) => {
-  getStudents({ res, file: './db/DiemHe4_K17.json' });
+app.get("/api/diemhe4_k17", async ({ res }) => {
+  getStudents({ res, file: "./db/DiemHe4_K17.json" });
 });
 
-app.get('/api/diemhe4_k18/:id', async (req, res) => {
+app.get("/api/diemhe4_k18/:id", async (req, res) => {
   const { id } = req.params;
-  getStudent({ id, res, file: './db/DiemHe4_K18.json' });
+  getStudent({ id, res, file: "./db/DiemHe4_K18.json" });
 });
 
-app.get('/api/diemhe4_k18', async ({ res }) => {
-  getStudents({ res, file: './db/DiemHe4_K18.json' });
+app.get("/api/diemhe4_k18", async ({ res }) => {
+  getStudents({ res, file: "./db/DiemHe4_K18.json" });
 });
 
-app.get('/api/diemhe4_k17__DTN', async (req, res) => {
-  getStudents({ res, file: './db/DiemHe4_K17__DTN.json' });
+app.get("/api/diemhe4_k17__DTN", async (req, res) => {
+  getStudents({ res, file: "./db/DiemHe4_K17__DTN.json" });
 });
 
-app.get('/api/diemhe4_k17__DTN/:id', async (req, res) => {
+app.get("/api/diemhe4_k17__DTN/:id", async (req, res) => {
   const { id } = req.params;
-  getStudent({ id, file: './db/DiemHe4_K17__DTN.json', res });
+  getStudent({ id, file: "./db/DiemHe4_K17__DTN.json", res });
 });
 
-app.get('/api/diemhe4_k18__DTN', async (req, res) => {
-  getStudents({ res, file: './db/DiemHe4_K18__DTN.json' });
+app.get("/api/diemhe4_k18__DTN", async (req, res) => {
+  getStudents({ res, file: "./db/DiemHe4_K18__DTN.json" });
 });
 
-app.get('/api/diemhe4_k18__DTN/:id', async (req, res) => {
+app.get("/api/diemhe4_k18__DTN/:id", async (req, res) => {
   const { id } = req.params;
-  getStudent({ id, res, file: './db/DiemHe4_K18__DTN.json' });
+  getStudent({ id, res, file: "./db/DiemHe4_K18__DTN.json" });
 });
 
-app.listen(PORT, () => console.log('Listen on port ' + PORT));
+app.listen(PORT, () => console.log("Listen on port " + PORT));
